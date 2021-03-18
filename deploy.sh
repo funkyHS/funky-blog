e2fd78f8724e6b73b9a5a9b9a6fc31b182d29a01
funkyHS <hs1024942667@163.com>
2021-03-18 20:16:32 +0800

    funky edit blog

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,12 @@ set -e
 # 生成静态文件
 npm run build
 
+# 后来添加，将原博客上传到master分支上
+git add .
+git commit -m 'funky edit blog'
+git push origin master
+
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
@@ -21,5 +27,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:funkyHS/funkyblog.git master:gh-pages
+
 
 cd -
